c96c8ea33c59dc21445108de478229009fd4e27a
ChristopheBlad <christopheblad@yahoo.fr>
2014-04-12 08:56:56 +0200

    commit complet

--- verifyRGS.sh
+++ verifyRGS.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
-#syntaxe verify "AC" "**" "signature" ou verify "porteur" "*" "authentification"
 #deux script, vérifier les certificats et vérifier les CRL
-#Faire peut-être un menu
 
 #Renommer les fichiers: issuerCN - subjectCN
 
-openssl x509 -text -noout -in $1 -inform PEM
+
 #essayer de trouver une chaine "Certificate:", si oui enregistrer, si non changer encodage
+openssl x509 -text -noout -in $1 -inform PEM > $1.txt
 
-echo "Le certificat s'affiche t'il correctement? (y/n)"
-
-read encodage
-
-if [ "$encodage" = "y" ]
+if grep "Certificate" $1.txt
 then
 	openssl x509 -text -noout -in $1 -inform PEM > $1.txt
     openssl asn1parse -in $1 -inform PEM > $1.asn1.txt
@@ -21,95 +16,118 @@ else
     openssl asn1parse -in $1 -inform DER > $1.asn1.txt
 fi
 
+echo "----------"
+echo "Certificat"
+echo "----------"
+grep "Subject:" $1.txt | sed -e "s/CN=/%/g" | cut -d'%' -f2 #coupe ce qui est apres
+grep -A 1 "commonName" $1.asn1.txt | while read line
+do
+    echo "$line"
+done
+grep -A 1 "X509v3 Subject Key Identifier" $1.txt
+
 #Vérifier la version
-if (grep "Version: 3" $1.txt)
-then
-    echo "Version OK (3)"
-else
-	echo "Version NOK (not 3)"
-fi
+echo "-----------------------------------------"
+echo "Vérifier la version x509: version 3 (0x2)"
+echo "-----------------------------------------"
+grep "Version" $1.txt
 
 #Vérifier Issuer
-# grep "Issuer:" $1.txt | sed -e "s/ //g"
+echo "-----------------------------------------------"
+echo "Vérifier que l'émetteur est bien l'AC qualifiée"
+echo "-----------------------------------------------"
 grep "Issuer:" $1.txt | sed -e "s/Issuer: /%/g" | cut -d'%' -f2 #coupe ce qui est apres
 
-
-#Vérifier algorithm de signature
-if (grep "Signature Algorithm: sha256WithRSAEncryption" $1.txt)
-then
-	echo "Signature Algorithm OK"
-else
-	if (grep "Signature Algorithm: sha1WithRSAEncryption" $1.txt)
-    then echo "WARNING: SHA1"
-    else echo "Signature Algorithm NOK"
-    fi
-fi
-
-exit 0
-
-
-grep "Signature Algorithm: sha256WithRSAEncryption"
-
-grep "Issuer:"
+#Vérifier algorithme de signature
+echo "----------------------------------"
+echo "Vérifier l'algorithme de signature"
+echo "----------------------------------"
+grep "Signature Algorithm" $1.txt
 
 #Validity
+echo "--------------------------------------------------------"
+echo "Vérifier la durée de vie (AC 10 ans, porteur 3 ans max.)"
+echo "--------------------------------------------------------"
+grep "Not Before" $1.txt
+grep "Not After" $1.txt
+
 
 #Subject dont encodage UTF8
+echo "-------------------------------------------------------------------------------------------------"
+echo "Référencement SGMAP - Vérifier l'encodage des champs (tout UTF8 sauf countryName PRINTABLESTRING)"
+echo "-------------------------------------------------------------------------------------------------"
+grep -A 1 "countryName" $1.asn1.txt
+grep -A 1 "commonName" $1.asn1.txt
 
-#Subject Public Key Info
-#Public Key Algorithm: rsaEncryption
-#Public-Key: (2048 bit)
-#Exponent: 65537 (0x10001)
+
+#Format Clé publique
+echo "-------------------------------------------------------"
+echo "Vérifier les informations de la clé publique du porteur"
+echo "-------------------------------------------------------"
+grep "Public Key Algorithm" $1.txt
+grep "Public-Key" $1.txt
+grep "Exponent" $1.txt
 
 #NOT Unique Identifiers
+echo "-------------------------------------------------"
+echo "Vérifier que Unique Identifiers n'est pas présent"
+echo "-------------------------------------------------"
+grep "Unique" $1.txt
 
 #Vérifier la présence des X509v3 extensions:
+echo "------------------------------------------"
+echo "Vérifier la présence des X509v3 extensions"
+echo "------------------------------------------"
+grep "X509v3 extensions" $1.txt | sed -e "s/x509 /%/g"
 
-grep "X509v3 Authority Key Identifier:"
+echo "-------------------"
+echo "Key Usage: Critical"
+echo "AC: cRLSign, keyCertSign"
+echo "Authentification: digitalSignature"
+echo "Signature, Cachet: nonRepudiation"
+echo "Confidentialité: dataEncipherment"
+echo "Authentification serveur: keyEncipherement"
+echo "-------------------"
+grep -A 1 "X509v3 Key Usage" $1.txt | sed -e "s/x509 /%/g"
 
-                keyid:5F:A8:71:60:BF:55:89:58:B5:E3:ED:20:99:E1:67:37:48:A9:B1:E1
+echo "-----------------------------------------------"
+echo "Basic Constraints (Critical si certificat d'AC)"
+echo "-----------------------------------------------"
+grep -A 1 "X509v3 Basic Constraints" $1.txt | sed -e "s/x509 /%/g"
 
+echo "------------------------"
+echo "Authority Key Identifier"
+echo "------------------------"
+grep -A 1 "X509v3 Authority Key Identifier" $1.txt | sed -e "s/x509 /%/g"
 
-grep "X509v3 Key Usage: critical"
+echo "--------------------"
+echo "Certificate Policies"
+echo "--------------------"
+grep -A 2 "X509v3 Certificate Policies" $1.txt | sed -e "s/x509 /%/g"
 
-Digital Signature
+echo "--------------------------------------------------"
+echo "Subject Alternative Name (optionnel): not critical"
+echo "--------------------------------------------------"
+grep -A 1 "X509v3 Subject Alternative Name" $1.txt | sed -e "s/x509 /%/g"
 
-            X509v3 Private Key Usage Period: 
+echo "--------------------------------------------------"
+echo "Issuer Alternative Name (optionnel): not critical"
+echo "--------------------------------------------------"
+grep -A 1 "X509v3 Issuer Alternative Name" $1.txt | sed -e "s/x509 /%/g"
 
-                Not Before: Oct  8 13:50:13 2013 GMT, Not After: Oct  8 13:50:13 2014 GMT
+echo "-----------------------"
+echo "CRL Distribution Points"
+echo "-----------------------"
+grep -A 4 "X509v3 CRL Distribution Points" $1.txt
 
-grep "X509v3 Certificate Policies:"
+echo "--------------------------------------"
+echo "Authority Information Access (si OCSP)"
+echo "--------------------------------------"
+grep -A 1 "Authority Information Access" $1.txt
 
-                Policy: 1.3.6.1.4.1.22234.2.8.3.5
+echo "------------"
+echo "QCStatements"
+echo "------------"
+grep -A 1 "QCStatements" $1.txt
 
-                  CPS: http://www.keynectis.com/PC/
-
-#les champs Subject Alternative Name et Issuer Alternative Name peuvent être présents, mais ils doivent obligatoirement être marqués "non critique" et être conformes aux exigences du chapitre 3.2.1 du [RFC3739].
-
-grep "X509v3 Basic Constraints: critical"
-
-                CA:FALSE
-
-            X509v3 Extended Key Usage: critical
-
-                Time Stamping
-
-
-
-grep "X509v3 CRL Distribution Points:"
-
-
-
-                Full Name:
-
-                  URI:http://trustcenter-crl.certificat2.com/Keynectis/Keynectis_CDS_CA_for_timestamping.crl
-
-
-
-                Full Name:
-
-                  URI:ldap://ldap.keynectis.com/CN=Keynectis%20CDS%20CA%20for%20timestamping,OU=KEYNECTIS%20CDS,OU=0002%20478217318,O=KEYNECTIS,C=FR?certificateRevocationList;binary?base?objectclass=pkiCA
-
-grep "Authority Information Access"
-
-QCStatements
+echo "*************************************************************"
