b0a46b1891101a9371334bc2707ea5a287288ed4
ChristopheBlad <christopheblad@yahoo.fr>
2014-04-13 15:28:10 +0200

    Ajout scripts CRL

--- verifyRGS.sh
+++ verifyRGS.sh
@@ -1,142 +1,198 @@
 #!/bin/sh
 #deux script, vérifier les certificats et vérifier les CRL
 
-#Renommer les fichiers: issuerCN - subjectCN
-for i in *.cer
+for i in certificats/*.cer
 do
 
-#essayer de trouver une chaine "Certificate:", si oui enregistrer, si non changer encodage
-openssl x509 -text -noout -in $i -inform PEM > $i.txt
+    #essayer de trouver une chaine "Certificate:", si oui enregistrer, si non changer encodage
+    openssl x509 -text -noout -in $i -inform PEM > $i.txt
 
-if grep "Certificate:" $i.txt
-then
-	openssl x509 -text -noout -in $i -inform PEM > $i.txt
-    openssl asn1parse -in $i -inform PEM > $i.asn1.txt
-else
-	openssl x509 -text -noout -in $i -inform DER > $i.txt
-    openssl asn1parse -in $i -inform DER > $i.asn1.txt
-fi
+    if grep "Certificate:" $i.txt
+    then
+    	openssl x509 -text -noout -in $i -inform PEM > $i.txt
+        openssl asn1parse -in $i -inform PEM > $i.asn1.txt
+    else
+    	openssl x509 -text -noout -in $i -inform DER > $i.txt
+        openssl asn1parse -in $i -inform DER > $i.asn1.txt
+    fi
 
-#Extraction du CN du porteur
-issuerCN=$(grep -A 1 "commonName" $i.asn1.txt | sed -n '2,2p' | sed 's/\(.*\):\(.*\):\(.*\):\(.*\)/\4/')
-subjectCN=$(grep -A 1 "commonName" $i.asn1.txt | sed -n '5,5p' | sed 's/\(.*\):\(.*\):\(.*\):\(.*\)/\4/')
+    #Extraction du CN du porteur
+    issuerCN=$(grep -A 1 "commonName" $i.asn1.txt | sed -n '2,2p' | sed 's/\(.*\):\(.*\):\(.*\):\(.*\)/\4/')
+    subjectCN=$(grep -A 1 "commonName" $i.asn1.txt | sed -n '5,5p' | sed 's/\(.*\):\(.*\):\(.*\):\(.*\)/\4/')
 
-filename=$(echo "$issuerCN - $subjectCN")
-mv $i.txt "$filename.txt"
-mv $i.asn1.txt "$filename.asn1.txt"
+    #Renommer les fichiers: issuerCN - subjectCN
+    filename=$(echo "$issuerCN - $subjectCN")
+    mv $i.txt "$filename.txt"
+    mv $i.asn1.txt "$filename.asn1.txt"
 
-#Subject
-echo "---------------------------------------------------------------------------------------------------"
-echo "Identification du sujet"
-echo "L'attribut countryName doit être présent. Il doit être renseigné en lettres majuscules."
-echo "L'attribut organizationName doit être présent et doit contenir le nom officiel complet de l'entité."
-echo "Une instance de l'attribut organizationalUnitName doit être présente: <00002> <SIRET/SIREN>."
-echo "L'attribut commonName doit comporter le premier prénom suivi d'un espace, suivi du nom du porteur."
-echo "il n'y a pas d'obligation à mentionner ces autres prénoms dans le certificat, mais s'ils le sont,"
-echo "ils doivent l'être dans le même ordre que sur la pièce d'identité et séparés par une virgule"
-echo "sans espace ni avant ni après la virgule."
-echo "Référencement SGMAP - Vérifier l'encodage des champs (tout UTF8 sauf countryName PRINTABLESTRING)"
-echo "---------------------------------------------------------------------------------------------------"
-grep -A 1 "countryName" "$filename.asn1.txt" | sed -n '4,5p'
-grep -A 1 "organizationName" "$filename.asn1.txt" | sed -n '4,5p'
-grep -A 1 "organizationalUnitName" "$filename.asn1.txt" | sed -n '4,5p'
-grep -A 1 "commonName" "$filename.asn1.txt" | sed -n '4,5p'
+    #Subject
+    echo "---------------------------------------------------------------------------------------------------"
+    echo "Identification du sujet"
+    echo "L'attribut countryName doit être présent. Il doit être renseigné en lettres majuscules."
+    echo "L'attribut organizationName doit être présent et doit contenir le nom officiel complet de l'entité."
+    echo "Une instance de l'attribut organizationalUnitName doit être présente: <00002> <SIRET/SIREN>."
+    echo "L'attribut commonName doit comporter le premier prénom suivi d'un espace, suivi du nom du porteur."
+    echo "il n'y a pas d'obligation à mentionner ces autres prénoms dans le certificat, mais s'ils le sont,"
+    echo "ils doivent l'être dans le même ordre que sur la pièce d'identité et séparés par une virgule"
+    echo "sans espace ni avant ni après la virgule."
+    echo "Référencement SGMAP - Vérifier l'encodage des champs (tout UTF8 sauf countryName PRINTABLESTRING)"
+    echo "---------------------------------------------------------------------------------------------------"
+    grep -A 1 "countryName" "$filename.asn1.txt" | sed -n '4,5p'
+    grep -A 1 "organizationName" "$filename.asn1.txt" | sed -n '4,5p'
+    grep -A 1 "organizationalUnitName" "$filename.asn1.txt" | sed -n '4,5p'
+    grep -A 1 "commonName" "$filename.asn1.txt" | sed -n '4,5p'
 
-#Vérifier la version
-echo "-----------------------------------------"
-echo "Vérifier la version x509: version 3 (0x2)"
-echo "-----------------------------------------"
-grep "Version" "$filename.txt"
+    #Vérifier la version
+    echo "-----------------------------------------"
+    echo "Vérifier la version x509: version 3 (0x2)"
+    echo "-----------------------------------------"
+    grep "Version" "$filename.txt"
 
-#Vérifier Issuer
-echo "-----------------------------------------------"
-echo "Vérifier que l'émetteur est bien l'AC qualifiée"
-echo "-----------------------------------------------"
-#grep "Issuer:" "$filename.txt" | sed -e "s/Issuer: /%/g" | cut -d'%' -f2 #coupe ce qui est apres
-grep "Issuer:" "$filename.txt"
+    #Vérifier Issuer
+    echo "-----------------------------------------------"
+    echo "Vérifier que l'émetteur est bien l'AC qualifiée"
+    echo "-----------------------------------------------"
+    #grep "Issuer:" "$filename.txt" | sed -e "s/Issuer: /%/g" | cut -d'%' -f2 #coupe ce qui est apres
+    grep "Issuer:" "$filename.txt"
 
-#Vérifier algorithme de signature
-echo "----------------------------------"
-echo "Vérifier l'algorithme de signature"
-echo "----------------------------------"
-grep "Signature Algorithm" "$filename.txt"
+    #Vérifier algorithme de signature
+    echo "----------------------------------"
+    echo "Vérifier l'algorithme de signature"
+    echo "----------------------------------"
+    grep "Signature Algorithm" "$filename.txt" | sed -n '2,2p'
 
-#Validity
-echo "--------------------------------------------------------"
-echo "Vérifier la durée de vie (AC 10 ans, porteur 3 ans max.)"
-echo "--------------------------------------------------------"
-grep "Not Before" "$filename.txt"
-grep "Not After" "$filename.txt"
+    #Validity
+    echo "--------------------------------------------------------"
+    echo "Vérifier la durée de vie (AC 10 ans, porteur 3 ans max.)"
+    echo "--------------------------------------------------------"
+    grep "Not Before" "$filename.txt"
+    grep "Not After" "$filename.txt"
 
-#Format Clé publique
-echo "-------------------------------------------------------"
-echo "Vérifier les informations de la clé publique du porteur"
-echo "-------------------------------------------------------"
-grep "Public Key Algorithm" "$filename.txt"
-grep "Public-Key" "$filename.txt"
-grep "Exponent" "$filename.txt"
+    #Format Clé publique
+    echo "-------------------------------------------------------"
+    echo "Vérifier les informations de la clé publique du porteur"
+    echo "-------------------------------------------------------"
+    grep "Public Key Algorithm" "$filename.txt"
+    grep "Public-Key" "$filename.txt"
+    grep "Exponent" "$filename.txt"
 
-#NOT Unique Identifiers
-echo "-------------------------------------------------"
-echo "Vérifier que Unique Identifiers n'est pas présent"
-echo "-------------------------------------------------"
-grep "Unique" "$filename.txt"
+    #NOT Unique Identifiers
+    echo "-------------------------------------------------"
+    echo "Vérifier que Unique Identifiers n'est pas présent"
+    echo "-------------------------------------------------"
+    grep "Unique" "$filename.txt"
 
-#Vérifier la présence des X509v3 extensions:
-echo "------------------------------------------"
-echo "Vérifier la présence des X509v3 extensions"
-echo "------------------------------------------"
-grep "X509v3 extensions" "$filename.txt" | sed -e "s/x509 /%/g"
+    #Vérifier la présence des X509v3 extensions:
+    echo "------------------------------------------"
+    echo "Vérifier la présence des X509v3 extensions"
+    echo "------------------------------------------"
+    grep "X509v3 extensions" "$filename.txt" | sed -e "s/x509 /%/g"
 
-echo "-------------------"
-echo "Key Usage: Critical"
-echo "AC: cRLSign, keyCertSign"
-echo "Authentification: digitalSignature"
-echo "Signature, Cachet: nonRepudiation"
-echo "Confidentialité: dataEncipherment"
-echo "Authentification serveur: keyEncipherement"
-echo "-------------------"
-grep -A 1 "X509v3 Key Usage" "$filename.txt" | sed -e "s/x509 /%/g"
+    echo "-------------------"
+    echo "Key Usage: Critical"
+    echo "AC: cRLSign, keyCertSign"
+    echo "Authentification: digitalSignature"
+    echo "Signature, Cachet: nonRepudiation"
+    echo "Confidentialité: dataEncipherment"
+    echo "Authentification serveur: keyEncipherement"
+    echo "-------------------"
+    grep -A 1 "X509v3 Key Usage" "$filename.txt" | sed -e "s/x509 /%/g"
 
-echo "-----------------------------------------------"
-echo "Basic Constraints (Critical si certificat d'AC)"
-echo "-----------------------------------------------"
-grep -A 1 "X509v3 Basic Constraints" "$filename.txt" | sed -e "s/x509 /%/g"
+    echo "-----------------------------------------------"
+    echo "Basic Constraints (Critical si certificat d'AC)"
+    echo "-----------------------------------------------"
+    grep -A 1 "X509v3 Basic Constraints" "$filename.txt" | sed -e "s/x509 /%/g"
 
-echo "------------------------"
-echo "Authority Key Identifier"
-echo "------------------------"
-grep -A 1 "X509v3 Authority Key Identifier" "$filename.txt" | sed -e "s/x509 /%/g"
+    echo "------------------------"
+    echo "Authority Key Identifier"
+    echo "------------------------"
+    grep -A 1 "X509v3 Authority Key Identifier" "$filename.txt" | sed -e "s/x509 /%/g"
 
-echo "--------------------"
-echo "Certificate Policies"
-echo "--------------------"
-grep -A 2 "X509v3 Certificate Policies" "$filename.txt" | sed -e "s/x509 /%/g"
+    echo "--------------------"
+    echo "Certificate Policies"
+    echo "--------------------"
+    grep -A 2 "X509v3 Certificate Policies" "$filename.txt" | sed -e "s/x509 /%/g"
 
-echo "--------------------------------------------------"
-echo "Subject Alternative Name (optionnel): not critical"
-echo "--------------------------------------------------"
-grep -A 1 "X509v3 Subject Alternative Name" "$filename.txt" | sed -e "s/x509 /%/g"
+    echo "--------------------------------------------------"
+    echo "Subject Alternative Name (optionnel): not critical"
+    echo "--------------------------------------------------"
+    grep -A 1 "X509v3 Subject Alternative Name" "$filename.txt" | sed -e "s/x509 /%/g"
 
-echo "--------------------------------------------------"
-echo "Issuer Alternative Name (optionnel): not critical"
-echo "--------------------------------------------------"
-grep -A 1 "X509v3 Issuer Alternative Name" "$filename.txt" | sed -e "s/x509 /%/g"
+    echo "--------------------------------------------------"
+    echo "Issuer Alternative Name (optionnel): not critical"
+    echo "--------------------------------------------------"
+    grep -A 1 "X509v3 Issuer Alternative Name" "$filename.txt" | sed -e "s/x509 /%/g"
 
-echo "-----------------------"
-echo "CRL Distribution Points"
-echo "-----------------------"
-grep -A 4 "X509v3 CRL Distribution Points" "$filename.txt"
+    echo "-----------------------"
+    echo "CRL Distribution Points"
+    echo "-----------------------"
+    grep -A 4 "X509v3 CRL Distribution Points" "$filename.txt"
 
-echo "--------------------------------------"
-echo "Authority Information Access (si OCSP)"
-echo "--------------------------------------"
-grep -A 1 "Authority Information Access" "$filename.txt"
+    echo "--------------------------------------"
+    echo "Authority Information Access (si OCSP)"
+    echo "--------------------------------------"
+    grep -A 1 "Authority Information Access" "$filename.txt"
 
-echo "------------"
-echo "QCStatements"
-echo "------------"
-grep -A 1 "QCStatements" "$filename.txt"
+    echo "------------"
+    echo "QCStatements"
+    echo "------------"
+    grep -A 1 "QCStatements" "$filename.txt"
 
-echo "*************************************************************"
+    echo "*****************************************************************************************"
+done
+
+# Vérification des CRL 
+for j in crl/*.crl
+do
+    echo $j
+    openssl crl -text -noout -inform DER -in $j > $j.txt
+    issuerCN=$(openssl asn1parse -inform DER -in $j | grep -A 1 "commonName" | sed -n '2,2p' | sed 's/\(.*\):\(.*\):\(.*\):\(.*\)/\4/')
+    echo "$issuerCN"
+    filename=$(echo "$issuerCN")
+    mv $j.txt "$filename.crl.txt"
+    #Version
+    echo "-----------------------------------------------------------------------------------------"
+    echo "Version: la valeur de ce champ doit être '1', indiquant qu'il s'agit d'une LCR version 2."
+    echo "-----------------------------------------------------------------------------------------"
+    grep "Version" "$filename.crl.txt"
+
+    #CRL Number
+    echo "----------"
+    echo "CRL Number"
+    echo "----------"
+    grep -A 1 "CRL Number" "$filename.crl.txt"
+
+    #Signature
+    echo "----------------------------------------------"
+    echo "Signature: Vérifier l'algorithme de signature."
+    echo "----------------------------------------------"
+    grep "Signature Algorithm" "$filename.crl.txt" | sed -n '2,2p'
+
+    #Issuer
+    echo "------"
+    echo "Issuer"
+    echo "------"
+    grep "Issuer:" "$filename.crl.txt"
+
+    #Authority Key Identifier
+    echo "------------------------"
+    echo "Authority Key Identifier"
+    echo "------------------------"
+    grep -A 1 "Authority Key Identifier" "$filename.crl.txt"
+
+    #Update
+    echo "----------------------------------------------------------------------------------------------------------------------------"
+    echo "This Update"
+    echo "Next Update. Il est recommandé que ce champ soit fonction de la fréquence de publication définie dans le document [RGS_A_13]"
+    echo "----------------------------------------------------------------------------------------------------------------------------"
+    grep "Update" "$filename.crl.txt"
+
+    #Revoked Certificates: userCertificate, revocationDate, crlEntryExtensions
+    echo "-------------------------------------------------------------------------"
+    echo "Revoked Certificates: userCertificate, revocationDate, crlEntryExtensions"
+    echo "Les raisons de révocation ne doivent pas être publiées (à vérifier)"
+    echo "-------------------------------------------------------------------------"
+    grep -A 2 "CRL entry extensions:" "$filename.crl.txt"
+    
+    echo "*****************************************************************************************"
 done
